1bad5dd2be7feff5e18f71023dc0949de5007dd0
AliSherNadeem <alisher786389@gmail.com>
2024-07-02 11:00:56 +0500

    add smooth scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import myImage from "../assets/My_image.png";
 import { MdArrowRightAlt } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -30,12 +31,17 @@ const Home = () => {
             to innovative projects.
           </p>
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-600 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-600 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <MdArrowRightAlt size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
 
